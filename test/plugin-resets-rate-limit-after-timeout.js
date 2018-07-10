83f817a46e2587a92d80ed9807a36f3589a04d78
Marcus Poehls <marcus.poehls@gmail.com>
2018-07-10 09:18:12 +0200

    add test for rate limit reset

---
+++ test/plugin-resets-rate-limit-after-timeout.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const Lab = require('lab')
+const Code = require('code')
+const Hapi = require('hapi')
+const Hoek = require('hoek')
+
+const server = new Hapi.Server()
+
+const Expect = Code.expect
+const { describe, it, before } = (exports.lab = Lab.script())
+
+describe('Send rate limit HTTP response headers,', () => {
+  before(async () => {
+    await server.register({
+      plugin: require('../lib/index'),
+      options: {
+        max: 1,
+        duration: 100, // 100ms
+        namespace: 'reset-response-headers'
+      }
+    })
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: () => {
+        return 'This is rate limitoooooooor!'
+      }
+    })
+
+    await server.initialize()
+  })
+
+  it('resets rate limit after window timeout', async () => {
+    const request = {
+      url: '/',
+      method: 'GET'
+    }
+
+    const response = await server.inject(request)
+    Expect(response.statusCode).to.equal(200)
+
+    const response2 = await server.inject(request)
+    Expect(response2.statusCode).to.equal(429)
+
+    await Hoek.wait(100)
+
+    const response3 = await server.inject(request)
+    Expect(response3.statusCode).to.equal(200)
+  })
+})
