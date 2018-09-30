718b2bf4c766b9da18f5221e5248cea3b612150a
Marcus Poehls <marcus.poehls@gmail.com>
2018-09-30 10:04:54 +0200

    add test: plugin works with defaults

---
+++ test/plugin-works-with-defaults.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const Test = require('ava')
+const Hapi = require('hapi')
+
+Test.beforeEach('Create server with rate limit defaults', async ({ context }) => {
+  const server = new Hapi.Server()
+
+  await server.register({
+    plugin: require('../lib'),
+    options: {
+      namespace: `route-limits-${Date.now()}`
+    }
+  })
+
+  await server.initialize()
+  context.server = server
+})
+
+Test('runs with rate limit defaults', async (t) => {
+  const server = t.context.server
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => {
+      return 'This is rate limitoooooooor!'
+    }
+  })
+
+  const request = {
+    url: '/',
+    method: 'GET'
+  }
+
+  const response = await server.inject(request)
+  t.is(response.statusCode, 200)
+  t.is(response.headers['x-rate-limit-limit'], 60)
+  t.is(response.headers['x-rate-limit-remaining'], 59)
+  t.not(response.headers['x-rate-limit-reset'], null)
+})
