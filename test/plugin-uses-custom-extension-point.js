1d4b970cd45d97c3d28abc6dbc34a1f2db1c5481
Marcus Poehls <marcus.poehls@gmail.com>
2018-12-12 09:58:07 +0100

    refine tests

--- test/plugin-uses-custom-extension-point.js
+++ test/plugin-uses-custom-extension-point.js
@@ -6,27 +6,39 @@ const Hapi = require('hapi')
 Test.beforeEach('Render view when rate limit is exceeded,', async ({ context }) => {
   const server = new Hapi.Server()
 
-  await server.register([
-    {
-      plugin: require('../lib'),
-      options: {
-        max: 20,
-        extensionPoint: 'onPreHandler',
-        namespace: `view-limits-${Date.now()}`
-      }
-    }])
+  await server.register(require('hapi-auth-basic'))
+
+  await server.auth.strategy('basic', 'basic', {
+    validate: () => {
+      return { isValid: false }
+    }
+  })
 
   await server.initialize()
   context.server = server
 })
 
-Test('plugin rate limits request onPreHandler', async (t) => {
+Test('plugin rate limits request onPreAuth', async (t) => {
   const server = t.context.server
+
+  await server.register([
+    {
+      plugin: require('../lib'),
+      options: {
+        max: 20,
+        extensionPoint: 'onPreAuth',
+        namespace: `view-limits-${Date.now()}`
+      }
+    }
+  ])
 
   server.route({
     method: 'GET',
     path: '/',
-    handler: () => 'This is rate limitoooooooor!'
+    options: {
+      auth: 'basic',
+      handler: () => 'This is rate limitoooooooor!'
+    }
   })
 
   const request = {
@@ -34,9 +46,45 @@ Test('plugin rate limits request onPreHandler', async (t) => {
     method: 'GET'
   }
 
-  const response1 = await server.inject(request)
-  t.is(response1.statusCode, 200)
-  t.is(response1.headers['x-rate-limit-limit'], 20)
-  t.is(response1.headers['x-rate-limit-remaining'], 19)
-  t.not(response1.headers['x-rate-limit-reset'], null)
+  const response = await server.inject(request)
+  t.is(response.statusCode, 401)
+  t.is(response.headers['x-rate-limit-limit'], 20)
+  t.is(response.headers['x-rate-limit-remaining'], 19)
+  t.not(response.headers['x-rate-limit-reset'], undefined)
+})
+
+Test('plugin does not rate limit at onPostAuth because auth fails', async (t) => {
+  const server = t.context.server
+
+  await server.register([
+    {
+      plugin: require('../lib'),
+      options: {
+        max: 20,
+        extensionPoint: 'onPostAuth',
+        namespace: `view-limits-${Date.now()}`
+      }
+    }
+  ])
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    options: {
+      auth: 'basic',
+      handler: () => 'This is rate limitoooooooor!'
+    }
+  })
+
+  const request = {
+    url: '/',
+    method: 'GET'
+  }
+
+  const response = await server.inject(request)
+
+  t.is(response.statusCode, 401)
+  t.is(response.headers['x-rate-limit-limit'], undefined)
+  t.is(response.headers['x-rate-limit-remaining'], undefined)
+  t.is(response.headers['x-rate-limit-reset'], undefined)
 })
