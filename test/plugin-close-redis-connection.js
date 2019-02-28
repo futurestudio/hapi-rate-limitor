647f8c38b30c544b81c11f17ab90b0d63ade9da9
Marcus Pöhls <marcus.poehls@gmail.com>
2019-02-28 13:51:28 +0100

    extend Redis connection tests

--- test/plugin-close-redis-connection.js
+++ test/plugin-close-redis-connection.js
@@ -2,6 +2,21 @@
 
 const Test = require('ava')
 const Hapi = require('hapi')
+const RateLimiter = require('../lib/rate-limiter')
+
+Test('Registers onPreStart and onPostStop extensions', async (t) => {
+  const server = new Hapi.Server()
+
+  await server.register({
+    plugin: require('../lib'),
+    options: {
+      namespace: `route-limits-${Date.now()}`
+    }
+  })
+
+  t.is(server._core.extensions.server.onPreStart._topo.nodes.length, 1)
+  t.is(server._core.extensions.server.onPostStop._topo.nodes.length, 1)
+})
 
 Test('Connects to Redis onPreStart and closes Redis connection onPostStop', async (t) => {
   const server = new Hapi.Server()
@@ -17,4 +32,14 @@ Test('Connects to Redis onPreStart and closes Redis connection onPostStop', asyn
   await server.stop()
 
   t.pass()
+})
+
+Test('Connect and disconnect from Redis on limiter start and stop', async (t) => {
+  const limiter = new RateLimiter({})
+  t.is(limiter.redis.status, 'wait')
+
+  await limiter.start()
+  t.is(limiter.redis.status, 'ready')
+
+  await limiter.stop()
 })
